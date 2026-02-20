1a6bfe5ef2df84dfba793d4e4b8c2c42e05f5ce1
Diinbadane <muniir669@gmail.com>
2026-02-20 23:33:23 +0300

    feat: add ci pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("hello devOps")
+console.log("i'm learning CI/CD using Github Actions")
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("start Testing...")
+setTimeout(()=>console.log("waiting 3 seconds"),3000)
+console.log("test completed")
